ea5723a4a5fe957776c8ff5be6c4d56b93e56a82
solankiomkar21-byte <solankiomkar21@gmail.com>
2026-04-05 23:36:49 +0530

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx' // Dhyan dein yahan ./App.jsx hi ho
+import App from './App.jsx' 
+import './index.css' // <--- Ye zaroori hai CSS load karne ke liye
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

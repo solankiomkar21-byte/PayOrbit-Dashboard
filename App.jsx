510245987fc2d3c97ebacf94b0143bd3b9975a1d
solankiomkar21-byte <solankiomkar21@gmail.com>
2026-04-05 23:26:50 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,4 +1,4 @@
-// In sab ko aise change karo:
+import React from 'react';
 import Sidebar from './Sidebar.jsx';
 import Navbar from './Navbar.jsx';
 import Dashboard from './Dashboard.jsx';
@@ -9,4 +9,21 @@ import VirtualAccounts from './VirtualAccounts.jsx';
 import Payouts from './Payouts.jsx';
 import Settings from './Settings.jsx';
 import { CurrencyProvider } from './CurrencyContext.jsx';
+
+function App() {
+  return (
+    <CurrencyProvider>
+      <div className="flex h-screen bg-gray-50">
+        <Sidebar />
+        <div className="flex-1 flex flex-col overflow-hidden">
+          <Navbar />
+          <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-50 p-6">
+            <Dashboard />
+          </main>
+        </div>
+      </div>
+    </CurrencyProvider>
+  );
+}
+
 export default App;

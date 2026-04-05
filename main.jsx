8717805c1372710b5eaf83d17967f702a8c6b7b0
solankiomkar21-byte <solankiomkar21@gmail.com>
2026-04-05 23:24:02 +0530

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx' // Dhyan dein yahan ./App.jsx hi ho
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)

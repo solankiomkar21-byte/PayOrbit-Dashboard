ce89f2a8920ebfc4245b9c663dcf8f01f20222de
solankiomkar21-byte <solankiomkar21@gmail.com>
2026-04-05 20:54:01 +0530

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import { CurrencyProvider } from './context/CurrencyContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CurrencyProvider>
-      <App />
-    </CurrencyProvider>
+    <App />
   </React.StrictMode>
 );
